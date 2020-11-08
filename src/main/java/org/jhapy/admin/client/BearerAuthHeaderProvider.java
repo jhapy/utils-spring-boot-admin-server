738e3aeaf48fdf4f8b5efb3fb08aedefc0643c44
Alexandre Clavaud <clavaud@icloud.com>
2020-11-08 21:29:28 +0100

    Upgrade Spring Boot Admin to 2.3.0

--- src/main/java/org/jhapy/admin/client/BearerAuthHeaderProvider.java
+++ src/main/java/org/jhapy/admin/client/BearerAuthHeaderProvider.java
@@ -1,14 +1,16 @@
 package org.jhapy.admin.client;
 import de.codecentric.boot.admin.server.domain.entities.Instance;
 import de.codecentric.boot.admin.server.web.client.HttpHeadersProvider;
+import org.jhapy.commons.utils.HasLogger;
 import org.springframework.http.HttpHeaders;
 import org.springframework.security.oauth2.client.OAuth2RestTemplate;
+
 /**
  * @author Alexandre Clavaud.
  * @version 1.0
  * @since 18/10/2020
  */
-public class BearerAuthHeaderProvider implements HttpHeadersProvider {
+public class BearerAuthHeaderProvider implements HttpHeadersProvider, HasLogger {
 
   private final OAuth2RestTemplate template;
 
@@ -17,8 +19,14 @@ public class BearerAuthHeaderProvider implements HttpHeadersProvider {
   }
 
   public HttpHeaders getHeaders(Instance ignored) {
+    String loggerPrefix = getLoggerPrefix("getHeaders", ignored);
+
+    logger()
+        .debug(loggerPrefix + "Generate Token of type " + template.getAccessToken().getTokenType());
+
     HttpHeaders headers = new HttpHeaders();
-    headers.set("Authorization", template.getAccessToken().getTokenType() + " " + template.getAccessToken().getValue());
+    headers.set("Authorization",
+        template.getAccessToken().getTokenType() + " " + template.getAccessToken().getValue());
     return headers;
   }
 }
